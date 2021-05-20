cffa78427039e987ba2004f12dae5010f0f9d963
melaniemonks <melaniemonks5@gmail.com>
2021-05-20 17:47:10 -0600

    initial commit

---
+++ week05/js/bom.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const unordered = document.querySelector('.list');
+
+button.addEventListener('click', () => {
+if (input.value != ''){
+	
+	let li = document.createElement('li');
+	let deletebutton = document.createElement('button');
+	
+	li.textContent = input.value;
+	deletebutton.textContent = '❌';
+	li.append(deletebutton);
+	unordered.append(li);
+	
+	deletebutton.addEventListener('click', function(){
+		unordered.removeChild(li);
+		input.focus;
+	})
+	
+	input.value='';
+	input.focus;
+}
+	
+	
+});
+
