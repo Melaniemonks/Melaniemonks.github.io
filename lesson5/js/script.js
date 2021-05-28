70066f8c81492b05e810c8ebe5888b841a1b4e58
melaniemonks <melaniemonks5@gmail.com>
2021-05-28 02:04:03 -0600

    initial commit

--- lesson5/js/script.js
+++ lesson5/js/script.js
@@ -1,16 +1,43 @@
 let thedate = new Date();
 console.thedate
-if (thedate.getDay() == 4) {
+if (thedate.getDay() == 5) {
     document.querySelector('#pancake').style.display = 'block';
 }
-
-
-let currentYear = "2021";
-let yearElement = document.querySelector('#year')
-yearElement.textContent = currentYear;
 
 function toggleMenu(){
     
     document.getElementById("navo").classList.toggle("navigation");
 }
 
+/* to get the current date*/ 
+let daysofweek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+let month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+let date = new Date();
+let day = daysofweek[date.getDay()];
+let months = month[date.getMonth()];
+year = date.getFullYear();
+let completeDate = day + ', ' + date.getDate() + ' ' + months + ' ' + year;
+document.querySelector('#presentDate').textContent = completeDate;
