30d187a48567f136b842572ceef43930087e3f10
melaniemonks <melaniemonks5@gmail.com>
2021-06-17 14:30:03 -0600

    initial commit

---
+++ lesson8/js/script.js
@@ -0,0 +1,11 @@
+function adjustRating(ranged) {
+    document.getElementById("rangevalue").innerHTML = ranged;
+}
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
