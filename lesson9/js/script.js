a47ab953d930c684c6f5b64a0ce834eed768a5c9
melaniemonks <melaniemonks5@gmail.com>
2021-06-29 15:22:21 -0600

    initial commit

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -6,13 +6,15 @@ fetch(requestURL)
     })
     .then(function (jsonObject) {
         //console.table(jsonObject);  // temporary checking for valid response and data parsing
-
+        console.log(jsonObject);
         const towns = jsonObject['towns'];
 
         for (let i = 0; i < towns.length; i++) {
 
             if (towns[i].name === "Soda Springs" || towns[i].name === "Fish Haven" || towns[i].name === "Preston") {
                 let info = document.createElement('section');
+                let writings = document.createElement('div');
+                let pictures = document.createElement('div');
                 let h2 = document.createElement('h2');
                 let h3 = document.createElement('h3');
                 let motto = document.createElement('p');
@@ -23,25 +25,83 @@ fetch(requestURL)
 
                 h2.innerHTML = `${towns[i].name} <span class="black"></span>`;
                 h3.innerHTML = `${towns[i].motto} <span class="black"></span>`;
-                year.innerHTML = `${towns[i].yearFounded} <span class="black"></span>`;
-                population.innerHTML = `${towns[i].currentPopulation} <span class="black"></span>`;
-                rainfall.innerHTML = `${towns[i].averageRainfall} <span class="black"></span>`;
-                img.setAttribute('src', towns[i].photo);
+                year.innerHTML = `Year Founded: ${towns[i].yearFounded} <span class="black"></span>`;
+                population.innerHTML = `Population: ${towns[i].currentPopulation} <span class="black"></span>`;
+                rainfall.innerHTML = `Annual Rain: ${towns[i].averageRainfall} <span class="black"></span>`;
+                img.setAttribute('src', "images/"+towns[i].photo);
+                img.setAttribute ('class', 'image1size')
                 img.setAttribute('Alt', `The offical portrait of ${towns[i].name}!`);
+                writings.setAttribute('class', 'writings');
+                info.setAttribute('class', 'info')
 
-                info.append(h2);
-                info.append(img);
-                info.append(h3);
-                info.append(motto);
-                info.append(year);
-                info.append(population);
-                info.append(rainfall);
+               
+
+                writings.append(h2);
+                writings.append(h3);
+                writings.append(motto);
+                writings.append(year);
+                writings.append(population);
+                writings.append(rainfall);
+                pictures.append(img)
+                info.append(pictures);
+                info.append(writings);
 
                 document.querySelector('div.individual').append(info);
             }
+
+            
         };
 
 
 
 
     });
+
+
+let thedate = new Date();
+console.thedate
+if (thedate.getDay() == 5) {
+    document.querySelector('#pancake').style.display = 'block';
+}
+
+
+function toggleMenu() {
+
+    document.getElementById("navo").classList.toggle("navigation");
+}
+
+
+/* to get the current date*/
+let daysofweek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+
+let month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+let date = new Date();
+let day = daysofweek[date.getDay()];
+let months = month[date.getMonth()];
+year = date.getFullYear();
+let completeDate = day + ', ' + date.getDate() + ' ' + months + ' ' + year;
+document.querySelector('#presentDate').textContent = completeDate;
+
+
