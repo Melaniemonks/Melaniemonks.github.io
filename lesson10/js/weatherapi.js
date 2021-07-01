6e7e8ac53a64d72c736db6723331f8725a41ac24
melaniemonks <melaniemonks5@gmail.com>
2021-07-01 09:11:42 -0600

    initial commit

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,6 +1,12 @@
-const apiURL = "api.openweathermap.org/data/2.5/weather?id=5604473&appid=41aad7e86df21cc463e072f37de54050";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=d58e26da4d9671ca99e0fbab6d30fb96";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
+
+    //this line using the current-temp ID and adds the main, temperature in the JSON file
+    document.getElementById('current-temp').textContent = jsObject.main.temp;  
+
   });
+
+  
