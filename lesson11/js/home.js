99c27ee62e2356e4d98c1eb687dc7455e8dcf5cf
melaniemonks <melaniemonks5@gmail.com>
2021-07-04 14:37:43 -0600

    initial commit

--- lesson11/js/home.js
+++ lesson11/js/home.js
@@ -1,15 +1,4 @@
 /*homepage Js*/
-let thedate = new Date();
-console.thedate
-if (thedate.getDay() == 5) {
-    document.querySelector('#pancake').style.display = 'block';
-}
-
-
-function toggleMenu() {
-
-    document.getElementById("navo").classList.toggle("navigation");
-}
 
 
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
@@ -42,13 +31,13 @@ fetch(requestURL)
                 year.innerHTML = `Year Founded: ${towns[i].yearFounded} <span class="black"></span>`;
                 population.innerHTML = `Population: ${towns[i].currentPopulation} <span class="black"></span>`;
                 rainfall.innerHTML = `Annual Rain: ${towns[i].averageRainfall} <span class="black"></span>`;
-                img.setAttribute('src', "images/"+towns[i].photo);
-                img.setAttribute ('class', 'image1size')
+                img.setAttribute('src', "images/" + towns[i].photo);
+                img.setAttribute('class', 'image1size')
                 img.setAttribute('Alt', `The offical portrait of ${towns[i].name}!`);
                 writings.setAttribute('class', 'writings');
                 info.setAttribute('class', 'info')
 
-               
+
 
                 writings.append(h2);
                 writings.append(h3);
@@ -61,6 +50,55 @@ fetch(requestURL)
                 info.append(writings);
 
                 document.querySelector('div.individual').append(info);
-            }     
+            }
         };
     });
+
+    let thedate = new Date();
+    console.thedate
+    if (thedate.getDay() == 5) {
+        document.querySelector('#pancake').style.display = 'block';
+    }
+    
+    
+    function toggleMenu() {
+    
+        document.getElementById("navo").classList.toggle("navigation");
+    }
+    
+    
+    /* to get the current date*/
+    let daysofweek = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+    ];
+    
+    let month = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    
+    let date = new Date();
+    let day = daysofweek[date.getDay()];
+    let months = month[date.getMonth()];
+    year = date.getFullYear();
+    let completeDate = day + ', ' + date.getDate() + ' ' + months + ' ' + year;
+    document.querySelector('#presentDate').textContent = completeDate;
+    
+    
+    
